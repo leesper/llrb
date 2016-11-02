84bb866fa0f5cb0cc3eddae159cd6b9e32958228
leesper <pascal7718@gmail.com>
2016-11-02 22:33:53 +0800

    llrb and llrb_test ok

--- llrb.go
+++ llrb.go
@@ -2,6 +2,7 @@ package llrb
 
 import (
   "fmt"
+  "strings"
 )
 
 type rbnode struct {
@@ -39,17 +40,71 @@ func rotateRight(node *rbnode) *rbnode {
   return leftChild
 }
 
+func moveRedLeft(node *rbnode) *rbnode {
+  node = colorFlip(node)
+  if isRed(node.right.left) {
+    node.right = rotateRight(node.right)
+    node = rotateLeft(node)
+    node = colorFlip(node)
+  }
+  return node
+}
+
+func moveRedRight(node *rbnode) *rbnode {
+  node = colorFlip(node)
+  if node.left != nil && isRed(node.left.left) {
+    node = rotateRight(node)
+    node = colorFlip(node)
+  }
+  return node
+}
+
+func deleteMax(node *rbnode) *rbnode {
+  if isRed(node.left) {
+    node = rotateRight(node)
+  }
+  if node.right == nil {
+    return nil
+  }
+  if !isRed(node.right) && !isRed(node.right.left) {
+    node = moveRedRight(node)
+  }
+  node.right = deleteMax(node.right)
+  return fixUp(node)
+}
+
+func deleteMin(node *rbnode) *rbnode {
+  if node.left == nil {
+    return nil
+  }
+  if !isRed(node.left) && !isRed(node.left.left) {
+    node = moveRedLeft(node)
+  }
+  node.left = deleteMin(node.left)
+  return fixUp(node)
+}
+
 func colorFlip(node *rbnode) *rbnode {
   node.red = !node.red
-
   if node.left != nil {
     node.left.red = !node.left.red
   }
-
   if node.right != nil {
     node.right.red = !node.right.red
   }
+  return node
+}
 
+func fixUp(node *rbnode) *rbnode {
+  if isRed(node.right) {
+    node = rotateLeft(node)
+  }
+  if isRed(node.left) && isRed(node.left.left) {
+    node = rotateRight(node)
+  }
+  if isRed(node.left) && isRed(node.right) {
+    node = colorFlip(node)
+  }
   return node
 }
 
@@ -57,12 +112,140 @@ func colorFlip(node *rbnode) *rbnode {
 type RBTree struct {
   root *rbnode
   length int
+  less func(a, b interface{}) bool
 }
 
-func NewRBTree() RBTree {
-  return RBTree{
-    root: new(rbnode),
+func NewRBTree(less func(a, b interface{}) bool) *RBTree {
+  return &RBTree{
+    less: less,
   }
+}
+
+func NewLowerCaseRBTree() *RBTree {
+  return &RBTree{less: func(a, b interface{}) bool {
+      return strings.ToLower(a.(string)) < strings.ToLower(b.(string))
+  }}
+}
+
+func NewIntRBTree() *RBTree {
+  return &RBTree{less: func(a, b interface{}) bool {
+    return a.(int) < b.(int)
+  }}
+}
+
+func NewFloat64RBTree() *RBTree {
+  return &RBTree{less: func(a, b interface{}) bool {
+    return a.(float64) < b.(float64)
+  }}
+}
+
+func (r *RBTree) Insert(k, v interface{}) bool {
+  ok := false
+  r.root, ok = r.insert(r.root, k, v)
+  r.root.red = false
+  if ok {
+    r.length++
+  }
+  return ok
+}
+
+func (r *RBTree) Find(k interface{}) (interface{}, bool) {
+  root := r.root
+  for root != nil {
+    if r.less(k, root.key) {
+      root = root.left
+    } else if r.less(root.key, k) {
+      root = root.right
+    } else {
+      return root.value, true
+    }
+  }
+  return nil, false
+}
+
+func (r *RBTree) insert(node *rbnode, k, v interface{}) (*rbnode, bool) {
+  ok := false
+  if node == nil {
+    return &rbnode{key: k, value: v, red: true}, true
+  }
+  if r.less(k, node.key) {
+    node.left, ok = r.insert(node.left, k, v)
+  } else if r.less(node.key, k) {
+    node.right, ok = r.insert(node.right, k, v)
+  } else {
+    node.value = v
+  }
+  return fixUp(node), ok
+}
+
+func (r *RBTree) Do(function func(interface{}, interface{})) {
+  do(r.root, function)
+}
+
+func (r *RBTree) Len() int {
+  return r.length
+}
+
+func do(node *rbnode, function func(interface{}, interface{})) {
+  if node != nil {
+    do(node.left, function)
+    function(node.key, node.value)
+    do(node.right, function)
+  }
+}
+
+func (r *RBTree) Delete(k interface{}) bool {
+  ok := false
+  if r.root != nil {
+    if r.root, ok = r.delete(r.root, k); r.root != nil {
+      r.root.red = false
+    }
+  }
+  if ok {
+    r.length--
+  }
+  return ok
+}
+
+func (r *RBTree) delete(node *rbnode, k interface{}) (*rbnode, bool) {
+  ok := false
+  if r.less(k, node.key) {
+    if node.left != nil {
+      if !isRed(node.left) && !isRed(node.left.left) {
+        node = moveRedLeft(node)
+      }
+      node.left, ok = r.delete(node.left, k)
+    }
+  } else {
+    if isRed(node.left) {
+      node = rotateRight(node)
+    }
+    if !r.less(k, node.key) && !r.less(node.key, k) && node.right == nil {
+      return nil, true
+    }
+    if node.right != nil {
+      if !isRed(node.right) && !isRed(node.right.left) {
+        node = moveRedRight(node)
+      }
+      if !r.less(k, node.key) && !r.less(node.key, k) {
+        smallest := min(node.right)
+        node.key = smallest.key
+        node.value = smallest.value
+        node.right = deleteMin(node.right)
+        ok = true
+      } else {
+        node.right, ok = r.delete(node.right, k)
+      }
+    }
+  }
+  return fixUp(node), ok
+}
+
+func min(node *rbnode) *rbnode {
+  for node.left != nil {
+    node = node.left
+  }
+  return node
 }
 
 func printInorderTree(node *rbnode, l int) {
--- llrb_test.go
+++ llrb_test.go
@@ -1,22 +1,111 @@
 package llrb
 
 import (
+  "strings"
   "testing"
 )
 
-func TestNewRBTree(t *testing.T) {
-  rbTree := NewRBTree()
-  if rbTree.root == nil {
-    t.Errorf("rbTree is nil, want non-nil")
-  }
-  if rbTree.root.right != nil {
-    t.Errorf("rbTree.root.right is non-nil, want nil")
-  }
+func TestStringKeyRBTreeInsertion(t *testing.T) {
+    wordForWord := NewLowerCaseRBTree()
+    for _, word := range []string{"one", "Two", "THREE", "four", "Five"} {
+        wordForWord.Insert(word, word)
+    }
+    var words []string
+    wordForWord.Do(func(_, value interface{}) {
+        words = append(words, value.(string))
+    })
+    actual, expected := strings.Join(words, ""), "FivefouroneTHREETwo"
+    if actual != expected {
+        t.Errorf("%q != %q", actual, expected)
+    }
 }
 
-func TestIsRed(t *testing.T) {
-  var nilNode *rbnode = nil
-  if isRed(nilNode) {
-    t.Errorf("nil node is red, want black")
-  }
+func TestIntKeyRBTreeFind(t *testing.T) {
+    intMap := NewIntRBTree()
+    for _, number := range []int{9, 1, 8, 2, 7, 3, 6, 4, 5, 0} {
+        intMap.Insert(number, number*10)
+    }
+    for _, number := range []int{0, 1, 5, 8, 9} {
+        value, found := intMap.Find(number)
+        if !found {
+            t.Errorf("failed to find %d", number)
+        }
+        actual, expected := value.(int), number*10
+        if actual != expected {
+            t.Errorf("value is %d should be %d", actual, expected)
+        }
+    }
+    for _, number := range []int{-1, -21, 10, 11, 148} {
+        _, found := intMap.Find(number)
+        if found {
+            t.Errorf("should not have found %d", number)
+        }
+    }
+}
+
+func TestIntKeyRBTreeDelete(t *testing.T) {
+    intMap := NewIntRBTree()
+    for _, number := range []int{9, 1, 8, 2, 7, 3, 6, 4, 5, 0} {
+        intMap.Insert(number, number*10)
+    }
+    if intMap.Len() != 10 {
+        t.Errorf("map len %d should be 10", intMap.Len())
+    }
+    length := 9
+    for i, number := range []int{0, 1, 5, 8, 9} {
+        if deleted := intMap.Delete(number); !deleted {
+            t.Errorf("failed to delete %d", number)
+        }
+        if intMap.Len() != length-i {
+            t.Errorf("map len %d should be %d", intMap.Len(), length-i)
+        }
+    }
+    for _, number := range []int{-1, -21, 10, 11, 148} {
+        if deleted := intMap.Delete(number); deleted {
+            t.Errorf("should not have deleted nonexistent %d", number)
+        }
+    }
+    if intMap.Len() != 5 {
+        t.Errorf("map len %d should be 5", intMap.Len())
+    }
+}
+
+func TestPassing(t *testing.T) {
+    intRBTree := NewIntRBTree()
+    intRBTree.Insert(7, 7)
+    passRBTree(intRBTree, t)
+}
+
+func passRBTree(r *RBTree, t *testing.T) {
+    for _, number := range []int{9, 3, 6, 4, 5, 0} {
+        r.Insert(number, number)
+    }
+    if r.Len() != 7 {
+        t.Errorf("should have %d items", 7)
+    }
+}
+
+// Thanks to Russ Cox for improving these benchmarks
+func BenchmarkRBTreeFindSuccess(b *testing.B) {
+    b.StopTimer() // Don't time creation and population
+    intRBTree := NewIntRBTree()
+    for i := 0; i < 1e6; i++ {
+        intRBTree.Insert(i, i)
+    }
+    b.StartTimer() // Time the Find() method succeeding
+    for i := 0; i < b.N; i++ {
+        intRBTree.Find(i % 1e6)
+    }
+}
+
+func BenchmarkRBTreeFindFailure(b *testing.B) {
+    b.StopTimer() // Don't time creation and population
+    intRBTree := NewIntRBTree()
+    for i := 0; i < 1e6; i++ {
+        intRBTree.Insert(2*i, i)
+    }
+    b.StartTimer() // Time the Find() method failing
+    for i := 0; i < b.N; i++ {
+        intRBTree.Find(2*(i%1e6) + 1)
+    }
 }
